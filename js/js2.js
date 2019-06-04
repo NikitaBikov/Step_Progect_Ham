c0b0da6a42ac099c1c7ff4450cf5c6b8acc020f1
Nikita <bikov.nikita2013@gmail.com>
2019-06-04 16:00:24 +0300

    add ham project

---
+++ js/js2.js
@@ -0,0 +1,34 @@
+$(function() {
+    $('.text-description').hide().first().show();
+    $('.team-member').hide().first().show();
+    $('.member-photo').hide().first().show();
+
+
+    $('.slider-photo').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        switchContent();
+    });
+
+    $('.slider-arrow.right').click(function(){
+        let activeIndex = $('.slider-photo.active').index() - 1;
+        let newIndex = (activeIndex == 3) ? 0 : activeIndex + 1;
+        $(`.slider-photo:eq(${activeIndex})`).animate({top: '+=25'}, 400).removeClass('active');
+        $(`.slider-photo:eq(${newIndex})`).animate({top: '-=25'}, 400).addClass('active');
+        switchContent();
+    });
+
+    $('.slider-arrow.left').click(function(){
+        let activeIndex = $('.slider-photo.active').index() - 1;
+        let newIndex = (activeIndex == 0) ? 3 : activeIndex - 1;
+        $(`.slider-photo:eq(${activeIndex})`).animate({top: '+=25'}, 400).removeClass('active');
+        $(`.slider-photo:eq(${newIndex})`).animate({top: '-=25'}, 400).addClass('active');
+        switchContent();
+    });
+
+    function switchContent () {
+        let activeIndex =  $('.slider-photo.active').index() - 1;
+        $(`.text-description:eq(${activeIndex})`).show().siblings('.text-description').hide();
+        $(`.team-member:eq(${activeIndex})`).show().siblings('.team-member').hide();
+        $(`.member-photo:eq(${activeIndex})`).show().siblings('.member-photo').hide();
+    }
+});
